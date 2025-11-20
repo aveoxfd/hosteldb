6d00a2341354dbf5be9b8972c099036526aadb1d
.ffd <krekerbruh@hotmail.com>
2025-11-20 12:29:16 +0300

    feat: add new buttons

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,19 @@ import javax.swing.*;
 public class Main {
     static String mainButtonsNames[] = {
         "Create a new list",
+        "Edit an entry",
         "Search",
-        "View full information"
+        "View full DB"
     };
 
-    static Task taskManager = new Task(10);
-
+    static JFrame frame = new JFrame("Hostelbd");
+    static Task taskManager = new Task(frame, 10);
+    
 
     public static void main(String args[]){ //main menu
-        JFrame frame = new JFrame("Hostel Database");
+        mainMenu();
+    }
+    private static void mainMenu(){
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400,400);
         frame.setVisible(true);
@@ -48,9 +52,14 @@ public class Main {
                     //TODO
                 });
             }
+            else if (mainMenuButtonNames=="Edit an entry"){
+                button.addActionListener(e -> {
+                    //TODO
+                });
+            }
 
             step++;
-            mainMenu.add(button);
+            taskManager.getTask().add(button);
         }
     }
     private static void searchRoom(){
@@ -59,16 +68,56 @@ public class Main {
         //TODO
     }
     private static void listRedactor(){
-        JPanel listRedactorPanel = new JPanel();
-        JTextField hostelNameField = new JTextField();
-        listRedactorPanel.setBounds(0,0,400,400);
-        listRedactorPanel.setLayout(null);
-        listRedactorPanel.setVisible(true);
+        String listRedactorButtons[] = {
+            "<-",
+            "Done"
+        };
 
+        JPanel listRedactorPanel = new JPanel();
+        taskManager.addTask(listRedactorPanel);
+        taskManager.getTask().setBounds(0,0,400, 400);
+        taskManager.getTask().setLayout(null);
+        taskManager.getTask().setVisible(true);
+
+        int step = 0;
+        for (String listRedactorButtonsParser : listRedactorButtons){
+            JButton button = new JButton(listRedactorButtonsParser);
+            if(listRedactorButtonsParser == "<-"){
+                button.setSize(45, 20);
+                button.setLocation(0, 0);
+            }
+            else if (listRedactorButtonsParser == "Done"){
+                button.setSize(200, 10);
+                button.setLocation(100, 350);
+            }
+            else{
+                button.setSize(200,50);
+                button.setLocation(100, 50+step*50);
+            }
+            if(listRedactorButtonsParser == "Done"){
+                button.addActionListener(e -> {
+                    System.out.println("Done");
+
+                    taskManager.getTask().setVisible(false);
+                    taskManager.removeTask();
+                    mainMenu();
+                });
+            }
+            if (listRedactorButtonsParser == "<-"){
+                button.addActionListener(e -> {
+                    taskManager.getTask().setVisible(false);
+                    taskManager.removeTask();
+                    mainMenu();
+                });
+            }
+            step++;
+            taskManager.getTask().add(button);
+        }
+        JTextField hostelNameField = new JTextField("Test");
         hostelNameField.setSize(200,30);
         hostelNameField.setLocation(100,50);
-        hostelNameField.setText("Hostel Name");
         //TODO
-        listRedactorPanel.add(hostelNameField);
+        taskManager.getTask().add(hostelNameField);
+        frame.add(taskManager.getTask());
     }
 }
